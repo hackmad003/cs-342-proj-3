a032b24a3a3b86118204a37932fe7aef9527a8ef
Ahmad Awaidah <130919187+hackmad003@users.noreply.github.com>
2025-11-23 16:56:06 -0600

    Add files via upload

---
+++ src/main/java/client/ClientApp.java
@@ -0,0 +1,26 @@
+package client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClientApp extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/welcome.fxml"));
+        primaryStage.setTitle("Three Card Poker");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+        
+        primaryStage.setOnCloseRequest(event -> {
+            System.exit(0);
+        });
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/server/Card.java
@@ -0,0 +1,71 @@
+package server; // For server project - change to 'client' for client project
+
+import java.io.Serializable;
+
+public class Card implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private char suit; // 'C', 'D', 'H', 'S'
+    private int value; // 2-14 (11=Jack, 12=Queen, 13=King, 14=Ace)
+    
+    public Card(char suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+    
+    public char getSuit() {
+        return suit;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    
+    public String getSuitName() {
+        switch(suit) {
+            case 'C': return "Clubs";
+            case 'D': return "Diamonds";
+            case 'H': return "Hearts";
+            case 'S': return "Spades";
+            default: return "Unknown";
+        }
+    }
+    
+    public String getValueName() {
+        switch(value) {
+            case 11: return "Jack";
+            case 12: return "Queen";
+            case 13: return "King";
+            case 14: return "Ace";
+            default: return String.valueOf(value);
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return getValueName() + " of " + getSuitName();
+    }
+    
+    // Short notation for display (e.g., "A♠", "K♥")
+    public String toShortString() {
+        String valueStr;
+        switch(value) {
+            case 11: valueStr = "J"; break;
+            case 12: valueStr = "Q"; break;
+            case 13: valueStr = "K"; break;
+            case 14: valueStr = "A"; break;
+            default: valueStr = String.valueOf(value);
+        }
+        
+        String suitSymbol;
+        switch(suit) {
+            case 'C': suitSymbol = "♣"; break;
+            case 'D': suitSymbol = "♦"; break;
+            case 'H': suitSymbol = "♥"; break;
+            case 'S': suitSymbol = "♠"; break;
+            default: suitSymbol = "?";
+        }
+        
+        return valueStr + suitSymbol;
+    }
+}
